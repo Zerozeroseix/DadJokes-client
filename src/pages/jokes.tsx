a08bb60a0ec92a4f02602180e4d9f2c8aabaddde
Zerozeroseix <mulomedicus@hotmail.com>
2023-04-27 12:47:36 +0200

    Small change in jokes.tsx

--- src/pages/jokes.tsx
+++ src/pages/jokes.tsx
@@ -1,4 +1,4 @@
-import {useCallback, useEffect, useRef, useState} from 'react';
+import {useCallback, useEffect, useState} from 'react';
 import Spinner from '@/components/spinner';
 import Timer from '@/components/timer';
 
